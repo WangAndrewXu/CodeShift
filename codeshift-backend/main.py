ec839857cc764ceed75bcf038a5a44d304fb12b5
Andrew Xu <andrewxuw@gmail.com>
2026-04-14 18:21:21 -0700

    Update main.py
    Change the local deploy to Internet deploy

--- codeshift-backend/main.py
+++ codeshift-backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://code-shift-sigma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
